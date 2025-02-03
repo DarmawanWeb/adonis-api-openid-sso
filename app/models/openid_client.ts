e8b64a34edc0a111d76d793a41a2c3a88cd403b6
Agus Darmawan <5024211070@mhs.its.ac.id>
2025-02-03 17:01:09 +0800

    feat: add openid client model

---
+++ app/models/openid_client.ts
@@ -0,0 +1,28 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@adonisjs/lucid/orm'
+
+export default class OpenidClient extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare name: string
+
+  @column()
+  declare clientId: string
+
+  @column()
+  declare clientSecret: string
+
+  @column()
+  declare redirectUri: string
+
+  @column()
+  declare allowedScopes: string
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+}
